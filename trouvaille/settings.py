9e9f1700b5b74e5355e24b90df5586537bc468ff
Carly Doppelheuer <carly.doppelheuer@gmail.com>
2024-02-14 17:41:26 -0600

    update settings

--- trouvaille/settings.py
+++ trouvaille/settings.py
@@ -13,8 +13,6 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from pathlib import Path
 import os
 from django.core.management.utils import get_random_secret_key
-from dotenv import load_dotenv
-load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
