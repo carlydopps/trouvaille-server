53782c82b1cb9a8afe99243b49549b16a6e0c907
Carly Doppelheuer <carly.doppelheuer@gmail.com>
2024-02-15 01:01:07 -0600

    update cors

--- trouvaille/settings.py
+++ trouvaille/settings.py
@@ -55,9 +55,9 @@ REST_FRAMEWORK = {
 }
 
 CORS_ORIGIN_WHITELIST = (
-    'http://127.0.0.1:8000',
     'http://localhost:3000',
-    'http://127.0.0.1:3000'
+    'http://127.0.0.1:3000',
+    'https://trouvaille-frontend.vercel.app',
 )
 
 MIDDLEWARE = [
